3d0cbcae8c7b34055e01a9a6cb830e7a38c1e25d
Pawan Chaudhary <pawan.email.np>
2020-11-29 22:54:25 +0545

    added images, modified ui, connected to api and sent form contact data to mongodb using post method

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'; 
+import axios from 'axios';
 import { Grid, TextField, Button} from '@material-ui/core';
+import SideImage from '../assets/side.png';
 export default class Contact extends Component {
     constructor(props){
         super(props);
@@ -10,29 +12,42 @@ export default class Contact extends Component {
             description:'',
         }
     }
+    handleChange = e=>{
+        this.setState({[e.target.name]: e.target.value})
+    }
+    handleSubmit = e =>{
+        e.preventDefault();
+        console.log(this.state);
+        axios.post('http://localhost:2000/feedbacks',this.state).then(res => {
+            console.log(res);
+        })
+        .catch(err =>{
+            console.log(err);
+        });
+    }
     render() {
         return(
             <div className="contact-page">
-            <Grid container spacing={3} style={{ position: 'relative', width: '650px', top: '100px', left: '2%'}}>
-            <div className='form-panel'></div>
-                <Grid item xs={12} sm={8}>
-                <TextField label="Name" name = "name" variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={8}>
-                <TextField label="Email" name = "email" type="email" variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={8}>
-                <TextField label="Subject" name = "subject" type="text" variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={8}>
-                <TextField multiline rows={9} variant="outlined" placeholder="Describe" fullWidth>Write a message</TextField>
-                </Grid>
-                <Grid item xs={12} sm={8}>
-      <Button fullWidth style={{backgroundColor: '#263238', color: 'white', padding:'20px'}} >Send</Button>
-                </Grid>
-            </Grid>
+                <Grid container spacing={4} style={{ position: 'relative', marginTop: '20px',backgroundColor:'white', width: '550px',minHeight:'646px' , top: '56px', left: '49.4%'}}>
+                    <div className='form-panel'></div>
+                        <Grid item xs={12}>
+                            <TextField label="Name" name = "name" variant="outlined" style={{marginTop:'30px'}}  fullWidth value={this.state.name} onChange={this.handleChange}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Email" name = "email" type="email" variant="outlined" fullWidth value={this.state.email} onChange={this.handleChange} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Subject" name = "subject" type="text" variant="outlined" fullWidth value={this.state.subject} onChange={this.handleChange} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField multiline rows={9} name="description" variant="outlined"  placeholder = "Describe" fullWidth value={this.state.description} onChange={this.handleChange}/>
+                        </Grid>
+                        <Grid item xs={12} >
+                            <Button fullWidth style={{backgroundColor: '#263238', color: 'white', padding:'20px'}} onClick={this.handleSubmit} >Send</Button>
+                        </Grid>
+                    </Grid>
+                <div className="image"> <img src= {SideImage} alt="no preview" width="700" height="650"/> </div>
             </div>
         );
     }
 }
-// <TextareaAutosize rowsMin={9} style={{width:'400px', backgroundColor: '#ede7f6', borderRadius:'7px', fontSize: '18px', fontFamily:'Arial', resize:'none', overflow: 'scroll'}} placeholder="Write a message" fullWidth />
